af5d5025783d2de5cd992e89924c5df619deb47b
jaykay12 <jalazkumar1208@gmail.com>
2017-11-10 01:25:32 +0530

    Dataset Furnish Script Added

---
+++ Dataset Preprocessing/Scripts/2 Data Furnishing/DataFurnish.py
@@ -0,0 +1,11 @@
+#Script to Prune the records Accordingly
+
+import pandas as pd
+
+for var in range(2000,2018):
+	newVar = str(var)
+	df = pd.read_csv('Records'+newVar+'.csv')
+	df.set_index('Scorecard',inplace=True)
+	newDF = df[['Team 1','Team 2','Winner','Margin','Ground','Match Date']]
+	newDF.to_csv('Records'+newVar+'.csv')
+	print("Done: Furnishing Records of "+newVar)
