c6787e4e81f9397ce8ceb2cc9afe3f22948933be
jaykay12 <jalazkumar1208@gmail.com>
2017-11-10 01:31:59 +0530

    Dataset Merger Script Added

---
+++ Dataset Preprocessing/Scripts/3 Dataset Merge/DataMerge.py
@@ -0,0 +1,13 @@
+#Merges different dataset files into a single CSV File
+
+import pandas as pd
+
+df = pd.read_csv('Records2000.csv')
+for var in range(2001,2018):
+	newVar = str(var)
+	dfCurrent = pd.read_csv('Records'+newVar+'.csv')
+	df = df.append(dfCurrent,ignore_index=True)
+	print("Merging Records of:"+newVar)
+
+df.set_index('Scorecard',inplace=True)
+df.to_csv('Dataset.csv')
