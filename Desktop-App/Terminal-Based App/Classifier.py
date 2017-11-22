39e33e0b02b66c653dafc51832d848e1c457595d
jaykay12 <jalazkumar1208@gmail.com>
2017-11-22 19:26:27 +0530

    Added Terminal Based Desktop App

---
+++ Desktop-App/Terminal-Based App/Classifier.py
@@ -0,0 +1,59 @@
+import datasetRead
+import dataInputFormat
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+
+d = datasetRead.Dataset()
+dIF = dataInputFormat.DataInput()
+
+class ourMLPClassifier:
+	def trainModel(self):
+		d.ReadDataSet()
+		self.MLPclf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(100, 32), random_state=1)
+		self.MLPclf.fit(d.X_train, d.Y_train)
+
+	def accuracyCheck(self):
+		print("\nMulti Layer Perceptron Classifer:")
+		test_predicted = self.MLPclf.predict(d.X_test)
+		print("Accuracy for Testing Dataset:",accuracy_score(d.Y_test, test_predicted))
+		train_predicted = self.MLPclf.predict(d.X_train)
+		print("Accuracy for Training Dataset:",accuracy_score(d.Y_train, train_predicted))
+
+	def runModel(self,inputPrediction,t1,t2):
+		ourPrediction = self.MLPclf.predict_proba([inputPrediction])
+
+		dIF.hashingTargetWinners()
+		totalPrediction = ourPrediction[0][dIF.winnerIndex[t1]] + ourPrediction[0][dIF.winnerIndex[t2]]
+		predictionT1 = (ourPrediction[0][dIF.winnerIndex[t1]]/totalPrediction) * 100
+		predictionT2 = (ourPrediction[0][dIF.winnerIndex[t2]]/totalPrediction) * 100
+		print("\n")
+		print(t1,":",predictionT1,"%")
+		print(t2,":",predictionT2,"%")
+
+
+class ourDTClassifier:
+	def trainModel(self):
+		d.ReadDataSet()
+		self.DTclf = tree.DecisionTreeClassifier()
+		self.DTclf.fit(d.X_train, d.Y_train)
+
+	def accuracyCheck(self):
+		print("\nDecision Tree Classifier:")
+		test_predicted = self.DTclf.predict(d.X_test)
+		print("Accuracy for Testing Dataset:",accuracy_score(d.Y_test, test_predicted))
+		train_predicted = self.DTclf.predict(d.X_train)
+		print("Accuracy for Training Dataset:",accuracy_score(d.Y_train, train_predicted))
+
+	def runModel(self,inputPrediction,t1,t2):
+		ourPrediction = self.DTclf.predict([inputPrediction])
+
+		dIF.hashingTargetWinners()
+		indexTeam1 = dIF.winnerIndex[t1]
+		indexTeam2 = dIF.winnerIndex[t2]
+		if ourPrediction[0][indexTeam1] == 1:
+		    print("Winner:",t1)
+		elif ourPrediction[0][indexTeam2] == 1:
+		    print("Winner:",t2)
+		else:
+		    print("Decision Tree Classifier Can't Predict for this match reliably!")
---
+++ Desktop-App/Terminal-Based App/main.py
@@ -0,0 +1,74 @@
+import sys
+from Classifier import ourMLPClassifier,ourDTClassifier
+import dataInputFormat
+
+if __name__ == '__main__':
+
+	MLPClf = ourMLPClassifier()
+	DTClf = ourDTClassifier()
+	dIF = dataInputFormat.DataInput()
+
+	print("Starting Up..!\n\nInitialising Models")
+	MLPClf.trainModel()
+	print("MLP Model:\tDone!")
+	MLPClf.accuracyCheck()
+	DTClf.trainModel()
+	print("Decision Tree Classifier:\tDone!")
+	DTClf.accuracyCheck()
+	dIF.hashAll()
+
+	while True:
+		choice = int(input("\n\nHit 0 to Exit & Hit 1 to proceed: "))
+		if choice == 0:
+			break
+
+		inputPrediction = [0]*217
+
+		t1 = input("Input Team 1: ")
+		t2 = input("Input Team 2: ")
+		ground = input("Input Ground: ")
+		innings = int(input("First Innings By\t[Team 1:1] [Team 2:2]: "))
+		venue = int(input("Venue\t[Home of Team 1:1] [Home of Team 2:2] [Neutral:3]: "))
+
+		if innings == 1:
+			inningsTeam1 = "Team1_1Inning"
+			inningsTeam2 = "Team2_2Inning"
+		elif innings == 2:
+			inningsTeam1 = "Team1_2Inning"
+			inningsTeam2 = "Team2_1Inning"
+		else:
+			print("Don't You Dare to CRASH the system!")
+			sys.exit()
+
+		if venue == 1:
+			venueTeam1 = "Team1_Home"
+			venueTeam2 = "Team2_Away"
+		elif venue == 2:
+			venueTeam1 = "Team1_Away"
+			venueTeam2 = "Team2_Home"
+		elif venue == 3:
+			venueTeam1 = "Team1_Neutral"
+			venueTeam2 = "Team2_Neutral"
+		else:
+			print("Don't You Dare to CRASH the system!")
+			sys.exit()	
+	
+		inputPrediction[dIF.ourTeams_1[t1]] = 1
+		inputPrediction[dIF.ourTeams_2[t2]] = 1
+		inputPrediction[dIF.ourGrounds[ground]] = 1
+		inputPrediction[dIF.ourInnings[inningsTeam1]] = 1
+		inputPrediction[dIF.ourInnings[inningsTeam2]] = 1
+		inputPrediction[dIF.ourVenues[venueTeam1]] = 1
+		inputPrediction[dIF.ourVenues[venueTeam2]] = 1
+
+		print("Menu:\n1- MLPClassifier\n2- DTClassifier\n3- Exit")
+		choice = int(input())
+		if choice==1:
+			MLPClf.runModel(inputPrediction,t1,t2)
+		elif choice==2:
+			DTClf.runModel(inputPrediction,t1,t2)
+		elif choice==3:
+			sys.exit()
+		else:
+			print("Don't You Dare to CRASH the system!")
+			sys.exit()
